cc87720d4b2d25e1f2c3138cbf61efeb82744327
Yurii Konyaev-Gurchenko <yuriikon56@gmail.com>
2019-07-20 15:38:29 +0400

    Added projects

---
+++ BRM.DA/Entities/BicycleStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Types;
+
+namespace BRM.DA.Entities
+{
+    public class BicycleStation
+    {
+        public int Id { get; set; }
+        public SqlGeography Location { get; set; }
+        public string Name { get; set; }
+    }
+}
